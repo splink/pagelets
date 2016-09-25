0d9b914579167ddfa6601ad2320145cc75c31f8f
Max Kugland <maxkugland@gmail.com>
2016-09-25 17:29:53 +0200

    add Visualizer tests

--- macros/src/main/scala/org/splink/raven/Visualizer.scala
+++ macros/src/main/scala/org/splink/raven/Visualizer.scala
@@ -1,13 +1,11 @@
 package org.splink.raven
 
-
 trait Visualizer {
   def visualizer: VisualizerService
 
   trait VisualizerService {
     def visualize(p: Pagelet): String
   }
-
 }
 
 trait VisualizerImpl extends Visualizer {
@@ -15,22 +13,22 @@ trait VisualizerImpl extends Visualizer {
     override def visualize(p: Pagelet) = {
       def rec(p: Pagelet, layer: Int = 0): String = p match {
         case t: Tree =>
-          val a = space(layer) + t.id + "\n"
+          val a = space(layer) + t.id.name + "\n"
           a + t.children.map(c => rec(c, layer + 1)).mkString
         case Leaf(id, fnc, _) =>
-          space(layer) + id + mkArgsString(fnc) + "\n"
+          space(layer) + id.name + mkArgsString(fnc) + "\n"
       }
 
       rec(p)
     }
 
-    def space(layer: Int) = (0 to layer).map(_ => "-").mkString
+    def space(layer: Int) = (1 to layer).map(_ => "-").mkString
 
     def mkArgsString(fnc: FunctionInfo[_]) =
       if (fnc.types.isEmpty) ""
       else "(" + fnc.types.map { case (name, typ) =>
         val index = typ.lastIndexOf(".")
-        name + ": " + (if (index > -1) typ.substring(index + 1) else typ)
+        name + ":" + (if (index > -1) typ.substring(index + 1) else typ)
       }.mkString(", ") + ")"
   }
 }
